6c42a5ff6ce1a3b4dafc14152285f09193c8e37f
innoflash <innocentmazando@gmail.com>
2019-10-24 15:37:47 +0200

    added IndexRequest

---
+++ src/Http/Requests/IndexRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FaithGen\SDK\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'limit' => 'integer|min:1',
+            'filter_text' => 'sometimes:string'
+        ];
+    }
+}
