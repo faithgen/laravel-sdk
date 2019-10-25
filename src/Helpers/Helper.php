413a83ff6a2616fd1f8aff6f95c0ca24fd71da33
innoflash <innocentmazando@gmail.com>
2019-10-25 12:06:28 +0200

    removed redundant helper item

--- src/Helpers/Helper.php
+++ src/Helpers/Helper.php
@@ -12,13 +12,7 @@ class Helper
     public static $titleValidation = 'required|string|min:2';
     public static $ministryBackend = 'http://localhost:8080';
 
-    public static $freeSermonsCount = 2;
-    public static $freeNewsCount = 2;
     public static $freeMessagesCount = 10;
-    public static $freeAlbumsCount = 1;
-    public static $freeAlbumImagesCount = 10;
-    public static $premiumAlbumsCount = 5;
-    public static $premiumAlbumImagesCount = 20;
 
     public static function getDates(Carbon $date)
     {
