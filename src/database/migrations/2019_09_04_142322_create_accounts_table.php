65c9895652eb8af1553b9e03ab568a807824c0cc
innoflash <innocentmazando@gmail.com>
2019-10-25 18:10:25 +0200

    added accounts migration

---
+++ src/database/migrations/2019_09_04_142322_create_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->string('id', 150)->primary();
+            $table->string('ministry_id', 150)->index();
+            $table->enum('level', ['Free', 'Premium', 'PremiumPlus'])->default('Free');
+            $table->dateTime('expiry_date')->default(now());
+            $table->timestamps();
+
+            $table->foreign('ministry_id')->references('id')->on('ministries')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
