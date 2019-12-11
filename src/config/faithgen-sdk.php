e76153a10bef758bc4fc265b43df1a1de9fca8ae
innoflash <innocentmazando@gmail.com>
2019-12-11 14:34:43 +0200

    updated larastart version

--- src/config/faithgen-sdk.php
+++ src/config/faithgen-sdk.php
@@ -15,7 +15,7 @@ return [
      * @Bool
      * If parent it exposes the update and delete routes, if not it will show only the select query routes
      */
-    'source' => false,
+    'source' => true,
 
     /**
      * @string
