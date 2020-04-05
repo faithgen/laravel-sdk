7faa5ee9e5996765aa36928a99d02b9c84171f10
innoflash <innocentmazando@gmail.com>
2020-04-05 19:26:45 +0200

    added broadcast support

--- src/Events/Commenter/UserPresent.php
+++ src/Events/Commenter/UserPresent.php
@@ -4,10 +4,11 @@ namespace FaithGen\SDK\Events\Commenter;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UserPresent
+class UserPresent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     /**
