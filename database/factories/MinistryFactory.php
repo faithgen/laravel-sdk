4db10bb21849a508fdddc8c987d3decb5d6f79ea
innoflash <innocentmazando@gmail.com>
2020-04-16 19:28:31 +0200

    added composer test

---
+++ database/factories/MinistryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use FaithGen\SDK\Models\Ministry;
+use Faker\Generator as Faker;
+
+$factory->define(Ministry::class, function (Faker $faker) {
+    return [
+
+    ];
+});
---
+++ tests/Feature/ComposerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace FaithGen\SDK\Feature;
+
+use Tests\TestCase;
+
+class ComposerTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function is_composer_setup_correct()
+    {
+        $composerData = json_decode(file_get_contents(__DIR__.'/../../composer.json'));
+
+        $this->assertFalse($composerData->name == 'faithgen/sdkk');
+        $this->assertTrue($composerData->name == 'faithgen/sdk');
+
+        $this->assertArrayHasKey("homepage", (array) $composerData);
+
+        $this->assertEquals($composerData->homepage, 'https://github.com/faithgen/laravel-sdk');
+
+        $this->assertArrayNotHasKey("damnit", (array) $composerData);
+
+        $providers = $composerData->extra->laravel->providers;
+
+        $this->assertCount(3, $providers);
+    }
+}
