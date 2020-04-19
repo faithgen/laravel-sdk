a5028016c689f47b093e923cb6bfc8b1509fc520
innoflash <innocentmazando@gmail.com>
2020-04-19 13:15:22 +0200

    fixed loaction

--- src/Http/Resources/Profile.php
+++ src/Http/Resources/Profile.php
@@ -11,40 +11,43 @@ class Profile extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
+     *
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
-            'phone' => $this->phone,
-            'color' => $this->profile->color,
-            'active' => (bool) $this->activation->active,
-            'api_key' => $this->apiKey->api_key,
-            'avatar' => ImageHelper::getImage('profile', $this->image, config('faithgen-sdk.ministries-server')),
-            'date' => Helper::getDates($this->created_at),
-            'users' => [
+            'id'        => $this->id,
+            'name'      => $this->name,
+            'email'     => $this->email,
+            'phone'     => $this->phone,
+            'color'     => $this->profile->color,
+            'active'    => (bool) $this->activation->active,
+            'api_key'   => $this->apiKey->api_key,
+            'avatar'    => ImageHelper::getImage('profile', $this->image, config('faithgen-sdk.ministries-server')),
+            'date'      => Helper::getDates($this->created_at),
+            'users'     => [
                 'count' => $this->ministryUsers()->count(),
             ],
-            'location' => $this->profile->location,
-            'links' => [
-                'website' => $this->profile->website,
-                'facebook' => $this->profile->facebook,
-                'youtube' => $this->profile->youtube,
-                'twitter' => $this->profile->twitter,
+            'location'  => is_array($this->profile->location)
+                ? $this->profile->location
+                : json_decode($this->profile->location),
+            'links'     => [
+                'website'   => $this->profile->website,
+                'facebook'  => $this->profile->facebook,
+                'youtube'   => $this->profile->youtube,
+                'twitter'   => $this->profile->twitter,
                 'instagram' => $this->profile->instagram,
             ],
-            'contact' => [
+            'contact'   => [
                 'phones' => $this->phones,
                 'emails' => $this->emails,
             ],
-            'services' => DailyService::collection($this->services),
+            'services'  => DailyService::collection($this->services),
             'statement' => [
-                'vision' => $this->profile->vision,
-                'mission' => $this->profile->mission,
+                'vision'   => $this->profile->vision,
+                'mission'  => $this->profile->mission,
                 'about_us' => $this->profile->about_us,
             ],
         ];
