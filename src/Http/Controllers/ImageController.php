184041cec2e496454026d08d6c12255b3430b18e
innoflash <innocentmazando@gmail.com>
2020-06-20 05:30:26 +0200

    updated image docs

--- src/Http/Controllers/ImageController.php
+++ src/Http/Controllers/ImageController.php
@@ -18,11 +18,26 @@ class ImageController extends Controller
         $this->imageService = $imageService;
     }
 
+    /**
+     * Sends a comment to an image.
+     *
+     * @param \FaithGen\Gallery\Http\Requests\ImageCommentRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function comment(ImageCommentRequest $request)
     {
         return CommentHelper::createComment($this->imageService->getImage(), $request);
     }
 
+    /**
+     * Fetches the comments for this image.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \FaithGen\SDK\Models\Image $image
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function comments(Request $request, Image $image)
     {
         return CommentHelper::getComments($image, $request);
